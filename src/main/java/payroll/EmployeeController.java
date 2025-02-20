019d3b7dcf5732321e17142365094c3ea8f5ad23
Ramone Robertson <ramonerobertson10@gmail.com>
2025-02-20 11:56:40 +0900

    Finished GET mapping for Employee Controller

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -1,4 +1,30 @@
 package payroll;
 
-public class EmployeeController {
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+// @RestController tells Spring that this class is a controller
+@RestController
+class EmployeeController {
+
+    // initialize a repository
+    private final EmployeeRepository repository;
+
+    // Constructor method to initialize repository
+    EmployeeController(EmployeeRepository repository){
+        this.repository = repository;
+    }
+
+    // Handle GET request (all entities)
+    @GetMapping("/employees")
+    List<Employee> all(){
+        return repository.findAll();
+    }
 }
