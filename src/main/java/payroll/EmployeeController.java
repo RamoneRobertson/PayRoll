06c0af34a6e34cd2635b7cbe848ca7293e6b9445
Ramone Robertson <ramonerobertson10@gmail.com>
2025-02-22 18:34:29 +0900

    Finished crud actions for API

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -22,9 +22,48 @@ class EmployeeController {
         this.repository = repository;
     }
 
-    // Handle GET request (all entities)
+    // Handle GET request (GET all employees)
     @GetMapping("/employees")
     List<Employee> all(){
         return repository.findAll();
     }
+
+    // Handle POST request (create a new employee)
+    @PostMapping("/employees")
+    // When we use RequestBody Spring will automatically convert the JSON request into code to create a new employee object
+    Employee newEmployee(@RequestBody Employee newEmployee){
+        return repository.save(newEmployee);
+    }
+
+    // Single Item
+
+
+    // GET one employee
+    @GetMapping("/employees/{id}")
+    Employee getEmployeeById(@PathVariable Long id){
+        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+
+    // PUT (Change/Update an employee)
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
+        // Update the name and role if an employee is found
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                // If an employee is not found create a new employee
+                .orElseGet(() -> {
+                    return repository.save(newEmployee);
+                });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        // Delte employye by finding the matching ID
+        repository.deleteById(id);
+    }
+
 }
